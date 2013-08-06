bb6d1e272cf76a35568330157c857f15ec7b3f58
Julio C. Menendez <hello@juliomenendez.com>
2013-08-06 06:20:17 -0600

    Removes unnecessary indentation in some templates.

--- templates/javascript/router.js
+++ templates/javascript/router.js
@@ -1,11 +1,11 @@
 define([
 	'backbone'
-	],
-	function(<%= _.classify('backbone') %>){
-        'use strict';
+],
+function(<%= _.classify('backbone') %>){
+    'use strict';
 
-		return Backbone.Router.extend({
-			/* Backbone routes hash */
-			routes: {}
-  		});
+	return Backbone.Router.extend({
+		/* Backbone routes hash */
+		routes: {}
+	});
 });
