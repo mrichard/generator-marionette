bdf99e1e75c3ac27a652e6f3ddb68e5bb166d046
Michel Richard <mrichard4@lm3000026417.s5at49.saksinc.com>
2013-08-24 13:48:51 -0400

    fix issue with latest yeoman generator

--- app/index.js
+++ app/index.js
@@ -1,15 +1,18 @@
 var util = require('util');
 var path = require('path');
 var yeoman = require('yeoman-generator');
-
+var chalk = require('chalk');
 
 module.exports = Generator;
 
 function Generator(args, options, config) {
   yeoman.generators.Base.apply(this, arguments);
 
-  this.testFramework = this.options['test-framework'] || 'mocha';
+  // custom mocha generator
+  this.testFramework = 'mocha-amd';
+
   this.templateFramework = 'handlebars';
+
   this.hookFor(this.testFramework, { as: 'app' });
 
   this.on('end', function () {
@@ -28,19 +31,24 @@ Generator.prototype.askFor = function askFor() {
   var welcome =
   '\n     _-----_' +
   '\n    |       |' +
-  '\n    |'+'--(o)--'.red+'|   .--------------------------.' +
-  '\n   `---------´  |    '+'Welcome to Yeoman,'.yellow.bold+'    |' +
-  '\n    '+'( '.yellow+'_'+'´U`'.yellow+'_'+' )'.yellow+'   |   '+'ladies and gentlemen!'.yellow.bold+'  |' +
+  '\n    |' + chalk.red('--(o)--') + '|   .--------------------------.' +
+  '\n   `---------´  |    ' + chalk.yellow.bold('Welcome to Yeoman') + ',    |' +
+  '\n    ' + chalk.yellow('(') + ' _' + chalk.yellow('´U`') + '_ ' + chalk.yellow(')') + '   |   ' + chalk.yellow.bold('ladies and gentlemen!') + '  |' +
   '\n    /___A___\\   \'__________________________\'' +
-  '\n     |  ~  |'.yellow +
-  '\n   __'+'\'.___.\''.yellow+'__' +
-  '\n ´   '+'`  |'.red+'° '+'´ Y'.red+' `\n';
+  '\n     ' + chalk.yellow('|  ~  |') +
+  '\n   __' + chalk.yellow('\'.___.\'') + '__' +
+  '\n ´   ' + chalk.red('`  |') + '° ' + chalk.red('´ Y') + ' `\n';
 
   console.log(welcome);
   console.log('Out of the box I include HTML5 Boilerplate, jQuery, Backbone.js, Marionette, Handlebars, Require and Modernizr.');
 
 
   var prompts = [{
+    type: 'confirm',
+    name: 'isFullApp',
+    message: 'Would you like to install the full express app or simply the marionette generators?'
+  },
+  {
     type: 'confirm',
     name: 'useMongoose',
     message: 'Would you like to include MongoDB for storage?'
@@ -54,14 +62,22 @@ Generator.prototype.askFor = function askFor() {
     type: 'confirm',
     name: 'useBaucis',
     message: 'Would you like to include Baucis for REST?'
+  },
+  {
+    type: 'string',
+    name: 'bowerDirectory',
+    message: 'Where do you want the Bower components installed?',
+    default: 'bower_components'
   }];
 
   this.prompt(prompts, function (props) {
     // manually deal with the response, get back and store the results.
     // we change a bit this way of doing to automatically do this in the self.prompt() method.
+    this.isFullApp = props.isFullApp;
     this.useMongoose = props.useMongoose;
     this.useSocketIO = props.useSocketIO;
     this.useBaucis = props.useBaucis;
+    this.bowerDirectory = props.bowerDirectory;
 
     //dummy vars for legacy
     this.compassBootstrap = true;
@@ -72,29 +88,31 @@ Generator.prototype.askFor = function askFor() {
 };
 
 Generator.prototype.git = function git() {
-  this.copy('gitignore', '.gitignore');
-  this.copy('gitattributes', '.gitattributes');
+  if( this.isFullApp ) {
+    this.template('gitignore', '.gitignore');
+    this.copy('gitattributes', '.gitattributes');
+  }
 };
 
 Generator.prototype.bower = function bower() {
-  this.copy('bowerrc', '.bowerrc');
-  this.copy('_bower.json', 'bower.json');
+    this.template('bowerrc', '.bowerrc');
+    this.copy('_bower.json', 'bower.json');
 };
 
 Generator.prototype.jshint = function jshint() {
-  this.copy('jshintrc', '.jshintrc');
+  if( this.isFullApp ) {
+    this.copy('jshintrc', '.jshintrc');
+  }
 };
 
 Generator.prototype.editorConfig = function editorConfig() {
-  this.copy('editorconfig', '.editorconfig');
+  if( this.isFullApp ) {
+    this.copy('editorconfig', '.editorconfig');
+  }
 };
 
 Generator.prototype.gruntfile = function gruntfile() {
-  if (this.testFramework === 'jasmine') {
-    this.write('Gruntfile.js', this.engine(this.read('Gruntfile.js'), this).replace(/mocha/g, 'jasmine'));
-  } else {
-    this.template('Gruntfile.js');
-  }
+  this.template('Gruntfile.js');
 };
 
 Generator.prototype.packageJSON = function packageJSON() {
@@ -102,45 +120,57 @@ Generator.prototype.packageJSON = function packageJSON() {
 };
 
 Generator.prototype.mainStylesheet = function mainStylesheet() {
-  if (this.compassBootstrap) {
-    this.write('app/styles/main.scss', '@import \'sass-bootstrap/lib/bootstrap\';\n\n.hero-unit {\n    margin: 50px auto 0 auto;\n    width: 400px;\n}');
-  } else {
-    this.write('app/styles/main.css', 'body {\n    background: #fafafa;\n}\n\n.hero-unit {\n    margin: 50px auto 0 auto;\n    width: 300px;\n}');
+  if( this.isFullApp ) {
+    if (this.compassBootstrap) {
+      this.write('app/styles/main.scss', '@import \'sass-bootstrap/lib/bootstrap\';\n\n.hero-unit {\n    margin: 50px auto 0 auto;\n    width: 400px;\n}');
+    } else {
+      this.write('app/styles/main.css', 'body {\n    background: #fafafa;\n}\n\n.hero-unit {\n    margin: 50px auto 0 auto;\n    width: 300px;\n}');
+    }
   }
 };
 
 
 Generator.prototype.bootstrapJs = function bootstrapJs() {
-  // TODO: create a Bower component for this
+  var _rootDir = this.isFullApp ? 'app/' : '';
+
   if (this.includeRequireJS && this.compassBootstrap) {
-    this.copy('bootstrap.js', 'app/scripts/vendor/bootstrap.js');
+    this.copy('bootstrap.js', _rootDir + 'scripts/vendor/bootstrap.js');
   }
 };
 
 Generator.prototype.setupEnv = function setupEnv() {
-  this.mkdir('app/templates');
+  var _rootDir = this.isFullApp ? 'app/' : '';
 
-  this.mkdir('server');
+  // templates
+  this.mkdir( _rootDir + 'templates' );
+  this.copy( 'app/welcome.hbs', _rootDir + 'templates/welcome.hbs');
 
-  this.mkdir('app');
-  this.mkdir('app/scripts');
-  this.mkdir('app/scripts/vendor/');
-  this.mkdir('app/styles');
-  this.mkdir('app/images');
-  this.template('app/404.html');
-  this.template('app/favicon.ico');
-  this.template('app/robots.txt');
-  this.copy('app/htaccess', 'app/.htaccess');
+  // server
+  if( this.isFullApp ) {
+    this.mkdir('server');
+    this.template('server/app.js', 'server/app.js');
+  }
 
-  this.template('server/app.js', 'server/app.js');
+  //html
+  this.template( 'app/index.html', _rootDir + 'index.html' );
 
-  this.copy( 'app/index.html', 'app/index.html' );
-  this.copy( 'app/main.js', 'app/scripts/main.js' );
-  this.copy( 'app/init.js', 'app/scripts/init.js' );
-  this.copy( 'app/regionManager.js', 'app/scripts/regionManager.js' );
-  this.copy( 'app/application.js', 'app/scripts/application.js' );
-  this.copy( 'app/communicator.js', 'app/scripts/communicator.js' );
+  // js
+  this.mkdir( _rootDir + 'scripts' );
+  this.copy( 'app/main.js', _rootDir + 'scripts/main.js' );
+  this.template( 'app/init.js', _rootDir + 'scripts/init.js' );
+  this.copy( 'app/regionManager.js', _rootDir + 'scripts/regionManager.js' );
+  this.copy( 'app/application.js', _rootDir + 'scripts/application.js' );
+  this.copy( 'app/communicator.js', _rootDir + 'scripts/communicator.js' );
 
-  this.copy( 'app/welcome.hbs', 'app/templates/welcome.hbs');
+  // other
+  if( this.isFullApp ) {
+    this.mkdir('app/styles');
+    this.mkdir('app/images');
+    this.template('app/404.html');
+    this.template('app/favicon.ico');
+    this.template('app/robots.txt');
+    this.copy('app/htaccess', 'app/.htaccess');
+  }
+
 };
 
