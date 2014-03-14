0ce8f4b73301b7c5461eabe31b041673622e4ed8
Leko <leko.noor@gmail.com>
2014-03-15 04:13:16 +0900

    collectionコマンドを実装

--- collection/index.js
+++ collection/index.js
@@ -1,14 +1,24 @@
 /*jshint latedef:false */
 var generator  = require('yeoman-generator');
+var fs         = require("fs");
 var util       = require('util');
 var path       = require('path');
 var validDir = require('../helpers/validateDirectory');
 
 module.exports = Generator;
 
+/**
+ * expected command variation
+ * 1. `$ yo backfire-ts:collection hoge`
+ * 2. `$ yo backfire-ts:collection with-model --model foo`
+ * 3. `$ yo backfire-ts:collection inherit-with-model --model foo --inherit bar`
+ */
 function Generator() {
+  var pkg = JSON.parse(fs.readFileSync('package.json', "utf8"));
+  this.appName = pkg.name.toLowerCase();
+
   generator.NamedBase.apply(this, arguments);
-  var dirPath = '../templates/javascript';
+  var dirPath = '../templates/typescript';
   this.sourceRoot(path.join(__dirname, dirPath));
 
   this.argument('model', { type: String, required: false });
@@ -21,17 +31,17 @@ function Generator() {
   }
 
   // invoke  mocha
-  this.hookFor('mocha-amd', { 
-    as: 'unitTest', 
-    args: [this.name, 'collection', 'collections']
-  });
+  // this.hookFor('mocha-amd', { 
+  //   as: 'unitTest', 
+  //   args: [this.name, 'collection', 'collections']
+  // });
 }
 
 util.inherits(Generator, generator.NamedBase);
 
 Generator.prototype.createViewFiles = function createViewFiles() {
-  var ext = 'js';
+  var ext = 'ts';
   var baseDir = validDir.getValidatedFolder( 'app/' );
 
-  this.template('collection.' + ext, path.join(baseDir + 'scripts/collections', this.name + '.' + ext));
+  this.template('collection.' + ext, path.join(baseDir + 'source/javascripts/'+this.appName+'/collection', this.name + '.' + ext));
 };
