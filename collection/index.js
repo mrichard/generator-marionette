ace6f02e5a2b1b41377d8fd954d4acd10b46fda7
Michel Richard <mrichard4@LM3000026417.local>
2014-04-09 18:25:01 -0400

    using prompts on model and collection generators

--- collection/index.js
+++ collection/index.js
@@ -2,34 +2,94 @@
 var generator  = require('yeoman-generator');
 var util       = require('util');
 var path       = require('path');
+var _          = require('underscore');
 var validDir = require('../helpers/validateDirectory');
 
 module.exports = Generator;
 
 function Generator() {
-  generator.NamedBase.apply(this, arguments);
+  generator.generators.NamedBase.apply(this, arguments);
   var dirPath = '../templates/javascript';
   this.sourceRoot(path.join(__dirname, dirPath));
 
-  this.argument('model', { type: String, required: false });
-  this.argument('inherit', { type: String, required: false });
 
-  this.option('create-all', { desc: 'Create a new model for this collection' });
 
-  if (this.model && this.options['create-all']) {
+  //this.argument('model', { type: String, required: false });
+ // this.argument('inherit', { type: String, required: false });
+
+  //this.option('create-all', { desc: 'Create a new model for this collection' });
+
+  /*if (this.model && this.options['create-all']) {
     this.hookFor('marionette', { as: 'model', args: [this.model, this.inherit], options: this.options });
-  }
+  }*/
 
   // invoke  mocha
   this.hookFor('mocha-amd', { 
     as: 'unitTest', 
     args: [this.name, 'collection', 'collections']
   });
-}
+};
 
 util.inherits(Generator, generator.NamedBase);
 
-Generator.prototype.createViewFiles = function createViewFiles() {
+
+// prompts
+Generator.prototype.askFor = function askFor() {
+  var cb = this.async();
+
+  // ask about inheriting, collection model, and are we creating the model
+  var prompts = [{
+    name: 'inherit',
+    message: 'Collection to inherit from?',
+    default: 'none'
+  },
+  {
+    name: 'model',
+    message: 'What is the ' + this.name + ' collection\'s model?',
+    default: 'none'
+  },
+  {
+    type: 'confirm',
+    name: 'createAll',
+    message: 'Do you need the model also created?',
+    default: true
+  }];
+
+  this.prompt(prompts, function (props) {
+    // manually deal with the response, get back and store the results.
+    // we change a bit this way of doing to automatically do this in the self.prompt() method.
+
+    // save inherit
+    if( props.inherit === 'none' ) {
+      this.inherit = null;
+    }else {
+      this.inherit = props.inherit;
+    }
+
+    // save model
+    if( props.model === 'none' ) {
+      this.model = null;
+    }else {
+      this.model = props.model;
+    }
+
+    // save create-all
+    this.createAll = props.createAll;
+
+    // try running model if createAll and model present
+    if( this.createAll && this.model ) {
+      var modelOptions = _.clone(this.options);
+      modelOptions.args = [ this.model ];
+      this.invoke( "marionette:model", modelOptions );
+    }
+
+    cb();
+  }.bind(this));
+
+};
+
+
+Generator.prototype.createCollectionFiles = function createCollectionFiles() {
   var ext = 'js';
   var baseDir = 'public/';
 
--- model/index.js
+++ model/index.js
@@ -2,28 +2,52 @@
 var generator  = require('yeoman-generator');
 var util       = require('util');
 var path       = require('path');
+var _          = require('underscore');
 var validDir = require('../helpers/validateDirectory');
 
 
 module.exports = Generator;
 
 function Generator() {
-  generator.NamedBase.apply(this, arguments);
+  generator.generators.NamedBase.apply(this, arguments);
   var dirPath = '../templates/javascript';
   this.sourceRoot(path.join(__dirname, dirPath));
 
-  this.argument( 'model', { type: String, required: false });
-  this.argument('inherit', { type: String, required: false });
+  //this.argument( 'model', { type: String, required: false });
+  //this.argument('inherit', { type: String, required: false });
 
   // invoke  mocha
-  this.hookFor('mocha-amd', { 
-    as: 'unitTest', 
+  this.hookFor('mocha-amd', {
+    as: 'unitTest',
     args: [this.name, 'model', 'models']
   });
-  
-}
+};
 
 util.inherits(Generator, generator.NamedBase);
+
+Generator.prototype.askFor = function askFor() {
+  var cb = this.async();
+
+  var prompts = [{
+    name: 'inherit',
+    message: 'Model to inherit from?',
+    default: 'none'
+  }];
+
+  this.prompt(prompts, function (props) {
+    // manually deal with the response, get back and store the results.
+    // we change a bit this way of doing to automatically do this in the self.prompt() method.
+
+    if( props.inherit !== 'none' ) {
+      this.inherit = props.inherit;
+    }else {
+      this.inherit = null;
+    }
+
+    cb();
+  }.bind(this));
+
+};
 
 Generator.prototype.createModelFiles = function createModelFiles() {
   var ext = 'js';
